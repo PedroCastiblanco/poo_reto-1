ad5fbaea34fe3d55720e403d9bedb688d6988e46
PedroCastiblanco <pcastblanco@unal.edu.co>
2024-11-20 14:28:38 -0500

    Update reto 1-1.py

--- reto 1-1.py
+++ reto 1-1.py
@@ -1,4 +1,4 @@
-a=input()
+a=input("Ingrese los digitos y el simbolo de la operacion: ")
 b=[]
 c=0
 for i in range(len(a)):
@@ -9,13 +9,13 @@ for i in range(len(a)):
         b.append(a[-2])
     
 if b[-1]=="+":
-    print(b[0]+b[1])
+    print("La suma entre "+str(b[0])+" y "+str(b[1])+" es: "+str(b[0]+b[1]))
 if b[-1]=="-":
-    print(b[0]-b[1])
+    print("La resta entre "+str(b[0])+" y "+str(b[1])+" es: "+str(b[0]-b[1]))
 if b[-1]=="*":
-    print(b[0]*b[1])
+    print("La multiplicación entre "+str(b[0])+" y "+str(b[1])+" es: "+str(b[0]*b[1]))
 if b[-1]=="/":
-    print(b[0]/b[1])
+    print("La division entre "+str(b[0])+" y "+str(b[1])+" es: "+str(b[0]/b[1]))
 
 
 
